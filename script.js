8bc2f7fae9eb3cdc55b227e8788793b51102330b
MHD224-Code <diallodyha1234@gmail.com>
2026-03-28 12:51:42 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    const contactBtn = document.getElementById("whatsapp-btn");
+    const today = new Date();
+    const curHr = today.getHours();
+    let messagePrefix;
+
+    if (curHr < 18) {
+        messagePrefix = "Bonjour";
+    } else {
+        messagePrefix = "Bonsoir";
+    }
+
+    const base_url = "https://wa.me/623 77 01 66"; // Remplace ici par ton numéro WhatsApp final
+    const full_message = `${messagePrefix} MHD-TECH, j’ai besoin d’une assistance pour...`;
+    
+    contactBtn.href = `${base_url}?text=${encodeURIComponent(full_message)}`;
+};
